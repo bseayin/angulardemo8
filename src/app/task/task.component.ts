8bb56e7de8a23db5b950594311509d22f0b0ea34
chenjia <625427490@qq.com>
2018-11-11 14:26:41 +0800

    cj

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../shared.service';
+import {TaskService} from './task.service';
+import { Task } from '../task';
 
 @Component({
   selector: 'app-task',
@@ -7,11 +9,49 @@ import { SharedService } from '../shared.service';
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  i = 1;
+  editCache = {};
+  dataSet : Task[];
 
-  constructor(private sharedService:SharedService) { }
+  constructor(private sharedService: SharedService,private taskservice: TaskService) { }
 
+  findTask(): void {
+    this.taskservice.findtasks()
+      .subscribe(
+        taskservice => {
+          this.dataSet = taskservice;
+          this.updateEditCache();
+
+        });
+  }
   ngOnInit() {
     this.sharedService.eventEmit.emit("任务");
+    this.findTask();
   }
 
+  startEdit(id: number): void {
+    this.editCache[id].edit = true;
+  }
+
+  cancelEdit(id: number): void {
+    this.editCache[id].edit = false;
+  }
+
+  saveEdit(id: number): void {
+    const index = this.dataSet.findIndex(item => item.id === id);
+    Object.assign(this.dataSet[index], this.editCache[id].data);
+    // this.dataSet[ index ] = this.editCache[ id ].data;
+    this.editCache[id].edit = false;
+  }
+
+  updateEditCache(): void {
+    this.dataSet.forEach(item => {
+      if (!this.editCache[item.id]) {
+        this.editCache[item.id] = {
+          edit: false,
+          data: { ...item }
+        };
+      }
+    });
+  }
 }
