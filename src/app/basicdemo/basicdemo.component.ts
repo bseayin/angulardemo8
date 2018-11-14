44cb964a879830fb3d2d21d842b6ffc7a8d11157
xijian chen <xijian>
2018-11-14 20:46:46 +0800

    cxj

--- src/app/basicdemo/basicdemo.component.ts
+++ src/app/basicdemo/basicdemo.component.ts
@@ -11,6 +11,7 @@ export class BasicdemoComponent implements OnInit {
   allClass: string = 'bb cc';
   // 布尔类型
   bbValue: boolean = false;
+  aaa;
 
   // 数组类型
   list: string[] = ['我的前半生','精彩绝伦','我的后半生','依然绚烂'];
@@ -20,15 +21,19 @@ export class BasicdemoComponent implements OnInit {
   name:string ="你今天咋了？";
   today: object = new Date();
   // 事件
-  todoClick(){
+  todoClick(test:any){
     alert('点我干嘛？');
     this.name="爱你所爱";
     this.show=false;
+    console.log("=====================aaaaa");
+    console.log(test)
+    console.log(test.target)
     }
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
