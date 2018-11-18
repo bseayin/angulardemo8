8e718259738e2476b8928ba841cd6eb30a15a4e4
yanyuhan <1406510041@qq.com>
2018-11-18 14:50:32 +0800

    鄢瑜晗

--- src/app/talk/talk.component.ts
+++ src/app/talk/talk.component.ts
@@ -11,19 +11,50 @@ import { TaskService } from '../task/task.service';
 export class TalkComponent implements OnInit {
   editCache = {};
   dataSet: Task[];
+  dataSet2: Task[];
+
+  
 
   constructor(private sharedService:SharedService, private taskservice: TaskService) { }
 
   ngOnInit() {
     this.sharedService.eventEmit.emit("讨论");
     this.findOwnPassTask();
+    this.findOwnNotPassTask();
   }
   findOwnPassTask(): void {
     this.taskservice.findOwnPassTasks()
       .subscribe(
         taskservice => {
-          this.dataSet = taskservice
+          console.log(this.dataSet2);
+          this.dataSet2 = taskservice
         });
+  }
+  
+  startEdit(id: number): void {
+    this.editCache[ id ].edit = true;
+  }
+
+  cancelEdit(id: number): void {
+    this.editCache[ id ].edit = false;
+  }
+
+  saveEdit(id: number): void {
+    const index = this.dataSet.findIndex(item => item.id === id);
+    Object.assign(this.dataSet[ index ], this.editCache[ id ].data);
+    // this.dataSet[ index ] = this.editCache[ id ].data;
+    this.editCache[ id ].edit = false;
+  }
+
+  updateEditCache(): void {
+    this.dataSet.forEach(item => {
+      if (!this.editCache[ item.id ]) {
+        this.editCache[ item.id ] = {
+          edit: false,
+          data: { ...item }
+        };
+      }
+    });
   }
 
 }
