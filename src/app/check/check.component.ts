a54398e9c12b7571d42a0655f042f971c122a305
yanyuhan <1406510041@qq.com>
2018-11-19 16:36:52 +0800

    yyh

--- src/app/check/check.component.ts
+++ src/app/check/check.component.ts
@@ -17,7 +17,62 @@ export class CheckComponent implements OnInit {
 
   ngOnInit() {
     this.sharedService.eventEmit.emit("审查");
+    this.findPassTask();
+    this.findNotPassTask();
+  }
+  ngOnInit2():void{
+    console.log("过");
+    this.findPassTask();
+    this.findNotPassTask();
+  }
+  findPassTask(): void {
+    this.taskservice.findPassTasks()
+      .subscribe(
+        taskservice => {
+           this.dataSet2 = taskservice
+           console.log("获取通过");
+           console.log(this.dataSet2);
+        });
+  }
 
+
+  findNotPassTask(): void {
+    this.taskservice.findNotPassTasks()
+      .subscribe(
+        taskservice => {
+          
+          this.dataSet = taskservice;
+          console.log("获取未通过");
+          console.log(this.dataSet);
+          this.updateEditCache();
+        });
+  }
+  
+  startEdit(id: number): void {
+    this.editCache[id].edit = true;
+  }
+
+  cancelEdit(id: number): void {
+    this.editCache[id].edit = false;
+  }
+
+  saveEdit(id: number): void {
+    const index = this.dataSet.findIndex(item => item.id === id);
+    Object.assign(this.dataSet[index], this.editCache[id].data);
+    // this.dataSet[ index ] = this.editCache[ id ].data;
+    this.editCache[id].edit = false;
+    this.taskservice.updateTask(this.editCache[id].data).subscribe(() =>this.ngOnInit2());
+  }
+
+  updateEditCache(): void {
+    this.dataSet.forEach(item => {
+      if (!this.editCache[item.id]) {
+        this.editCache[item.id] = {
+          edit: false,
+          data: { ...item }
+        };
+      }
+    });
   }
 
 }
